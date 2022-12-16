0ec95c1dcb15563c4c46fd39f70ce75d7732eebf
Ashwin Abraham <210050023@iitb.ac.in>
2022-12-17 02:49:01 +0530

    Added assignment 1

---
+++ Week1/README.md
@@ -0,0 +1,18 @@
+# Week 1
+
+In this week, we will learn about Python, in particular Python Libraries such an ```NumPy```, ```TensorFlow```, ```PyTorch```, ```MatPlotLib``` and ```SciKit-Learn```.
+
+## Assignment 1
+
+The notebook ```MNIST.ipynb``` present in this repository contains various from-scratch implementations of common Machine Learning Algorithms.
+
+Your task will be to rewrite each of these using the standard implementations of these algorithms using ```SciKit-Learn```.
+
+This repository also contains some starter code for creating a window and moving a square around in it. Modify this code to implement a game of [Snake](<https://en.wikipedia.org/wiki/Snake_(video_game_genre)>).
+
+### Notes
+
+- Do not worry if you don't fully understand the algorithms or their from-scratch implementations, these algorithms are mostly Supervised Learning algorithms which we will not use for RL. This assignment is purely to familiarize you with Python and the Libraries in it.
+- This project is intended to be highly collaborative, so get to know your co-mentees and work with them on this!
+- Learn to use ```git``` for version control while working with them
+- Submission instructions will be announced soon. The assignment should be finished within a week from today (17th December)
---
+++ Week1/snake.py
@@ -0,0 +1,59 @@
+import pygame
+from pygame.locals import *
+
+class Square(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        super(Square, self).__init__()
+        self.surf = pygame.Surface((20, 20))
+        self.surf.fill((0, 200, 255))
+        # self.rect = self.surf.get_rect()
+        self.pos = [x, y]
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+ 
+square = Square(40, 40)
+
+# Use blit to put something on the screen
+screen.blit(square.surf, tuple(square.pos))
+
+# Update the display using flip
+pygame.display.flip()
+
+
+gameOn = True
+# Our game loop
+while gameOn:
+    # for loop through the event queue
+    pygame.time.Clock().tick(60)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            gameOn = False
+    keys = pygame.key.get_pressed()
+    square.surf.fill((0, 0, 0))
+    screen.blit(square.surf, tuple(square.pos)) # Remove old square
+    square.surf.fill((0, 200, 255))
+    if keys[K_w] or keys[K_UP]:
+        square.pos[1] -= 10
+        # keys[K_w] = False
+        # keys[K_UP] = False
+    if keys[K_a] or keys[K_LEFT]:
+        square.pos[0] -= 10
+        # keys[K_a] = False
+        # keys[K_LEFT] = False
+    if keys[K_s] or keys[K_DOWN]:
+        square.pos[1] += 10
+        # keys[K_s] = False
+        # keys[K_DOWN] = False
+    if keys[K_d] or keys[K_RIGHT]:
+        square.pos[0] += 10
+        # keys[K_d] = False
+        # keys[K_RIGHT] = False
+    screen.blit(square.surf, tuple(square.pos)) # Put new square
+    # Update the display using flip
+    pygame.display.flip()
+
+pygame.quit()
