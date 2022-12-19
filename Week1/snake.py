fdec064cc52e442ece571fb4074a282879d927f3
Satyankar <satyankar@Satyankars-MacBook-Air.local>
2022-12-20 01:42:20 +0530

    multiple snake blocks added

--- Week1/snake.py
+++ Week1/snake.py
@@ -1,11 +1,14 @@
 import pygame
 from pygame.locals import *
 
+sq_size = 20
+move_size = sq_size*1.5
+
 class Square(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
         super(Square, self).__init__()
-        self.surf = pygame.Surface((20, 20))
+        self.surf = pygame.Surface((sq_size, sq_size))
         self.surf.fill((0, 200, 255))
         # self.rect = self.surf.get_rect()
         self.pos = [x, y]
@@ -16,20 +19,20 @@ pygame.init()
 W, H = 800, 600
 screen = pygame.display.set_mode((W, H))
  
-square = Square(40, 40)
-square2 = Square(40, 40)
+count = 5
+square = [Square(40, 40) for _ in range(count)]
 
-dirn = 0
+dirn = [0]*count
 
 move = {
-    0 : [0, 30],
-    1 : [30, 0],
-    2 : [0, -30],
-    3 : [-30, 0]
+    0 : [0, move_size],
+    1 : [move_size, 0],
+    2 : [0, -move_size],
+    3 : [-move_size, 0]
 }
 
 # Use blit to put something on the screen
-screen.blit(square.surf, tuple(square.pos))
+screen.blit(square[0].surf, tuple(square[0].pos))
 
 # Update the display using flip
 pygame.display.flip()
@@ -39,53 +42,40 @@ gameOn = True
 while gameOn:
     # for loop through the event queue
     pygame.time.Clock().tick(60)
+    pygame.time.wait(100)
     for event in pygame.event.get():
         if event.type == QUIT:
             gameOn = False
     keys = pygame.key.get_pressed()
 
-    print(square.pos)
+    print(square[0].pos, square[1].pos, dirn[-5:])
 
-    square.surf.fill((0, 0, 0))
-    screen.blit(square.surf, tuple(square.pos)) # Remove old square
-    square.surf.fill((0, 200, 255))
-    square2.surf.fill((0, 0, 0))
-    screen.blit(square2.surf, tuple(square2.pos)) # Remove old square2
-    square2.surf.fill((0, 200, 255))
+    for i in range(count):
+        square[i].surf.fill((0, 0, 0))
+        screen.blit(square[i].surf, tuple(square[i].pos)) # Remove old square[i]
+        square[i].surf.fill((0, 200, 255))
 
     if keys[K_w] or keys[K_UP]:
-        square.pos[1] -= 10
-        # keys[K_w] = False
-        # keys[K_UP] = False
-        dirn = 0
+        square[0].pos[1] -= move_size
+        dirn.append(0)
     elif keys[K_a] or keys[K_LEFT]:
-        square.pos[0] -= 10
-        # keys[K_a] = False
-        # keys[K_LEFT] = False
-        dirn = 1
+        square[0].pos[0] -= move_size
+        dirn.append(1)
     elif keys[K_s] or keys[K_DOWN]:
-        square.pos[1] += 10
-        # keys[K_s] = False
-        # keys[K_DOWN] = False
-        dirn = 2
+        square[0].pos[1] += move_size
+        dirn.append(2)
     elif keys[K_d] or keys[K_RIGHT]:
-        square.pos[0] += 10
-        # keys[K_d] = False
-        # keys[K_RIGHT] = False
-        dirn = 3
+        square[0].pos[0] += move_size
+        dirn.append(3)
 
-    square2.pos = [square.pos[i] + move[dirn][i] for i in range(2)]
-        
-    square.pos[0] %= W
-    square.pos[1] %= H
+    for i in range(1, count):
+        square[i].pos = [square[i-1].pos[j] + move[dirn[-i]][j] for j in [0, 1]]
 
-    square2.pos[0] %= W
-    square2.pos[1] %= H
-   
-
-
-    screen.blit(square.surf, tuple(square.pos)) # Put new square
-    screen.blit(square2.surf, tuple(square2.pos)) # Put new square2
+    for i in range(count):
+        square[i].pos[0] %= W
+        square[i].pos[1] %= H
+        screen.blit(square[i].surf, tuple(square[i].pos)) # Put new square[i]
+    
     # Update the display using flip
     pygame.display.flip()
 
