1edaf2ea9f2cf1d16c9e71e9bce4c99e2af9c6bc
Pratik Sahoo <22b0968@iitb.ac.in>
2023-01-13 18:57:56 +0530

    used swarnim for once

--- Week3/Assignment-1-Mountain-Car/mountain_car.py
+++ Week3/Assignment-1-Mountain-Car/mountain_car.py
@@ -94,11 +94,12 @@ class QAgent:
         First discretize both the state and next_state to get indices in q-table.
         The boolean is_terminal here represents whether the state action pair resulted in termination (NOT TRUNCATION) of environment. In this case, update the value by considering max_a' q(s', a,) = 0 (consult theory for why) and not based on q-table.
         '''
-        
+        state0 = self.get_state_index(state)
+        state1 = self.get_state_index(next_state)
         if is_terminal:
-            self.q_table[state[0]][state[1]][action] = 0
+            self.q_table[state0[0]][state0[1]][action] = 0
         else:
-            self.q_table[state[0]][state[1]][action] += self.alpha*(reward + self.gamma*np.maximum(self.q_table[next_state[0]][next_state[1]]) - self.q_table[state[0]][state[1]][action])
+            self.q_table[state0[0]][state0[1]][action] += self.alpha*(reward + self.gamma*np.maximum(self.q_table[state1[0]][state1[1]]) - self.q_table[state[0]][state[1]][action])
     
     def get_action(self):    
         '''
